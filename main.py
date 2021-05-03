bcfdf2841b6d54926799b9ac6b53c1c82af6e900
Melzak <melzacki.jakub@gmail.com>
2021-05-03 19:43:02 +0200

    hotfix to homework

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, Request, status, HTTPException
-from fastapi.responses import  HTMLResponse
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 import datetime
 
 app = FastAPI()
 app.tokens = []
 
+def generate_html_response():
+    html_content = f"""
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Hello! Today day is {datetime.date.today()}</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/hello")
 def root():
-    html_response = f"<h1>Hello! Today day is {datetime.date.today()}</h1>"
-    return HTMLResponse(content=html_response, status_code=200)
+    return generate_html_response()
 
 
