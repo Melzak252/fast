69f009b335d56717e5add85b0381b1ff20bb889e
Melzak <melzacki.jakub@gmail.com>
2021-05-03 20:07:06 +0200

    2. exc. from homework

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request, status, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, status, HTTPException, Cookie
+from fastapi.responses import HTMLResponse, Response
 from pydantic import BaseModel
 import datetime
 
 app = FastAPI()
 app.tokens = []
+app.session_token = "AutoryzacjaUzyskana"
 
 
 def generate_html_response():
@@ -24,3 +25,21 @@ def generate_html_response():
 @app.get("/hello", response_class=HTMLResponse)
 def root():
     return generate_html_response()
+
+
+@app.post("/login_session", status_code=201)
+def login(user: str, password: str, response: Response):
+    if user == "4dm1n" and password == "NotSoSecurePa$$":
+        response.set_cookie(key="session_token", value=app.session_token)
+        return {"message": "Zalogowano"}
+    else:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+
+
+@app.post("/login_token", status_code=201)
+def login_token(*, response: Response, session_token: str = Cookie(None)):
+    if session_token == app.session_token:
+        return {"message": app.session_token}
+    else:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Unauthorised")
+
