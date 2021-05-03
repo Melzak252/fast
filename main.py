25eb37d3e60ee799020151616a03cdbb7c03b75f
Melzak <melzacki.jakub@gmail.com>
2021-05-03 20:32:17 +0200

    2. exc. from homework hotfix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from fastapi import FastAPI, Request, status, HTTPException, Cookie
 from fastapi.responses import HTMLResponse, Response
 from pydantic import BaseModel
@@ -5,7 +7,8 @@ import datetime
 
 app = FastAPI()
 app.tokens = []
-app.session_token = "AutoryzacjaUzyskana"
+app.secret_key = "AutoryzacjaUzyskana"
+app.access_token = None
 
 
 def generate_html_response():
@@ -28,17 +31,23 @@ def root():
 
 
 @app.post("/login_session", status_code=201)
-def login(user: str, password: str, response: Response):
-    if user == "4dm1n" and password == "NotSoSecurePa$$":
+def login(login: str, password: str, response: Response):
+    if login == "4dm1n" and password == "NotSoSecurePa$$":
+
+        app.access_token = hashlib.sha256(f"{login}{password}{app.secret_key}".encode()).hexdigest()
+
         response.set_cookie(key="session_token", value=app.session_token)
         return {"message": "Zalogowano"}
+
     else:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
 
 @app.post("/login_token", status_code=status.HTTP_201_CREATED)
 def login_token(*, response: Response, session_token: str = Cookie("default")):
-    if session_token == app.session_token:
-        return {"token": app.session_token}
-    else:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Unauthorised")
+
+    if app.access_token and session_token:
+        if session_token == app.access_token:
+            return {"token": app.access_token}
+
+    raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Unauthorised")
