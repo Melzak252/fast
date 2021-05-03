6d17a5975c6e5d9a8b7838f367419bebcbed43e3
Melzak <melzacki.jakub@gmail.com>
2021-05-03 19:34:26 +0200

    added solution to first exc form homework

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request, status, HTTPException
+from fastapi.responses import  HTMLResponse
+from pydantic import BaseModel
+import datetime
+
+app = FastAPI()
+app.tokens = []
+
+
+@app.get("/hello")
+def root():
+    html_response = f"<h1>Hello! Today day is {datetime.date.today()}</h1>"
+    return HTMLResponse(content=html_response, status_code=200)
+
+
