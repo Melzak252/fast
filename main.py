3fdbca6c62f12bc1b5bed0d31f0927933f1411bc
Melzak <melzacki.jakub@gmail.com>
2021-05-04 08:49:21 +0200

    2. exc. from homework hotfix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 import secrets
-from fastapi import FastAPI, Request, status, HTTPException, Depends
+from fastapi import FastAPI, Request, status, HTTPException, Depends, Cookie
 from fastapi.responses import HTMLResponse, Response, JSONResponse
 from pydantic import BaseModel
 import datetime
@@ -36,7 +36,7 @@ def generate_html_response():
 
 
 @app.get("/hello", response_class=HTMLResponse)
-def root():
+async def hello():
     return generate_html_response()
 
 
@@ -46,13 +46,13 @@ def check_auth(credentials: HTTPBasicCredentials = Depends(security)):
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect login or password",
             headers={"WWW-Authenticate": "Basic"},
         )
 
 
 @app.post("/login_session", status_code=status.HTTP_201_CREATED)
-def login_session(credentials: HTTPBasicCredentials = Depends(security), ):
+async def login_session(credentials: HTTPBasicCredentials = Depends(security), ):
     check_auth(credentials)
     response = Response(status_code=status.HTTP_201_CREATED)
     response.set_cookie(key="session_token", value=app.access_session)
@@ -60,8 +60,54 @@ def login_session(credentials: HTTPBasicCredentials = Depends(security), ):
 
 
 @app.post("/login_token", status_code=status.HTTP_201_CREATED)
-def login_token(credentials: HTTPBasicCredentials = Depends(security), ):
+async def login_token(credentials: HTTPBasicCredentials = Depends(security), ):
     check_auth(credentials)
     response = JSONResponse(content={"token": app.access_token}, status_code=status.HTTP_201_CREATED)
     response.set_cookie(key="session_token", value=app.access_token)
     return response
+
+@app.get("/welcome_session")
+async def welcome_session(format: Optional[str] = None, session_token: Optional[str] = Cookie(None)):
+    if not session_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect session token",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if session_token != app.access_session:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect session token",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if format == "json":
+        return JSONResponse(content={"message": "Welcome!"})
+    elif format == "html":
+        return HTMLResponse(content="<h1>Welcome!</h1>")
+    else:
+        return Response(content="Welcome!")
+
+@app.get("/welcome_token")
+async def welcome_token(format: Optional[str] = None, token: Optional[str] = None):
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect session token",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if token != app.access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect session token",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    if format == "json":
+        return JSONResponse(content={"message": "Welcome!"})
+    elif format == "html":
+        return HTMLResponse(content="<h1>Welcome!</h1>")
+    else:
+        return Response(content="Welcome!")
